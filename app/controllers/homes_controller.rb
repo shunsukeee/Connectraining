cd6c45e388e935c777713c5a53f3433e4dc2dcfa
shunsuke <shunsuke.nakayama36@gmail.com>
2020-06-17 23:10:01 +0900

    [add]home

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,14 +1,15 @@
 class HomesController < ApplicationController
 	def home
-		
+		@users = User.order("RANDOM()").limit(4)
 	end
 
 	def torainer
-		
+		@users = User.where(user_status: 2)
+
 	end
 
 	def torainee
-		
+		@users = User.where(user_status: 1)
 	end
 
 	def about
