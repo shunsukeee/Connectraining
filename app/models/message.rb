189f7cae2b4141dbc79b1a321b90ffe834905eaa
shunsuke <shunsuke.nakayama36@gmail.com>
2020-06-25 03:03:10 +0900

    バリデーション記入

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,10 @@
 class Message < ApplicationRecord
 
-	validates :message, presence: true
+	# ======バリデーション===========================
+	validates :message, presence: true,
+                          length: { maximum: 100 }
+    # ============================================
+    
 	belongs_to :user
 	belongs_to :room
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,10 +4,19 @@ class Tweet < ApplicationRecord
 	attachment :tweet_image
 	has_many :tweet_comments, dependent: :destroy
 	has_many :favorites, dependent: :destroy
+
+    # =======バリデーション==========================
+    validates :body, presence: true,
+                          length: { maximum: 100 }
+    # =============================================
+
+    # =======いいね時のif文=============================
 	def favorited_by?(user)
         	favorites.where(user_id: user.id).exists?
     end
+    # ================================================
 
+    # =======検索======================================
     def Tweet.search(search, user_or_tweet)
     	if user_or_tweet == "2"
             Tweet.where(['body LIKE ?', "%#{search}%"])
@@ -15,4 +24,5 @@ class Tweet < ApplicationRecord
     		Tweet.all
     	end
     end
+    # =================================================
 end
