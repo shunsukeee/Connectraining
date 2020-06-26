c8db750fcfda381e3fccf1e1f17f15cfca301b5b
shunsuke <shunsuke.nakayama36@gmail.com>
2020-06-26 18:42:00 +0900

    mesage

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,11 +3,8 @@ class MessagesController < ApplicationController
 	before_action :authenticate_user!, only: [:create]
 
   def create
-    if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
-      @message = Message.create(params.require(:message).permit(:user_id, :message, :room_id).merge(user_id: current_user.id))
-    else
-      flash[:notice] = "メッセージ送信に失敗しました。"
-    end
+    Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
+    @message = Message.create(params.require(:message).permit(:user_id, :message, :room_id).merge(user_id: current_user.id))
     redirect_to room_path(@message.room)
   end
 end
