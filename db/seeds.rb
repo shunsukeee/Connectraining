375090ec2383fab52de5264693e58d5409e43bf1
shunsuke <shunsuke.nakayama36@gmail.com>
2020-07-17 17:23:55 +0900

    [add]seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+admin = Admin.new(:email => 'admin@com', :password => '111111')
+admin.save!
+
