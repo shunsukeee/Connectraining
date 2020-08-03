8986b5ea7d6d80d2f6428e4df65d8a52e3f34321
shunsuke <shunsuke.nakayama36@gmail.com>
2020-08-03 11:55:32 +0900

    rooms

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,6 @@ class MessagesController < ApplicationController
 	before_action :authenticate_user!, only: [:create]
 
   def create
-    Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
     @message = Message.create(params.require(:message).permit(:user_id, :message, :room_id).merge(user_id: current_user.id))
     redirect_to room_path(@message.room)
   end
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -8,8 +8,8 @@ class RoomsController < ApplicationController
 
   def create
     @room = Room.create
-    @entry1 = Entry.create(room_id: @room.id, user_id: current_user.id)
-    @entry2 = Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(room_id: @room.id))
+    @entry = Entry.create(room_id: @room.id, user_id: current_user.id)
+    @entryparams = Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(room_id: @room.id))
     redirect_to "/rooms/#{@room.id}"
   end
 
