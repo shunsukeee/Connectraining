21f4177b8bd3512e6b33c0b874971db8de3a3a86
shunsuke <shunsuke.nakayama36@gmail.com>
2020-08-15 19:10:34 +0900

    [fix]admins_controller

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -1,6 +1,5 @@
 class Admins::UsersController < ApplicationController
-
-	def index
-		@admin_users = User.page(params[:page]).reverse_order.per(8)
-	end
+  def index
+    @admin_users = User.page(params[:page]).reverse_order.per(8)
+  end
 end
