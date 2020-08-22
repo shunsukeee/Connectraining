5cd131f9409ed302ef78007d43efafce9fb0460c
shunsuke <shunsuke.nakayama36@gmail.com>
2020-08-22 20:25:15 +0900

    [fix]message_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,5 @@
 class MessagesController < ApplicationController
-
-	before_action :authenticate_user!, only: [:create]
+  before_action :authenticate_user!, only: [:create]
 
   def create
     @message = Message.create(params.require(:message).permit(:user_id, :message, :room_id).merge(user_id: current_user.id))
